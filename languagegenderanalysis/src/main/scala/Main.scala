b102287a9e414e8a0a2e4a8ad11a436fe9dba2ba
sunayna <30575613+sunayna-ray@users.noreply.github.com>
2023-05-09 01:57:16 -0500

    Add IPA Conversions

--- languagegenderanalysis/src/main/scala/Main.scala
+++ languagegenderanalysis/src/main/scala/Main.scala
@@ -1,16 +1,30 @@
 package lga
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, substring}
+
+import scala.io.Source
 
 object Main {
   def main(args: Array[String]): Unit = {
+
     val spark: SparkSession = SparkSession
       .builder()
       .master("local")
       .appName("LanguageGenderAnalysis")
       .getOrCreate()
 
-    println(spark.getClass())
+    //French IPA: https://unalengua.com/ipa-translate?hl=en&ttsLocale=fr-CA&voiceId=Chantal&sl=fr&text=
+    //Hindi IPA: https://www.fontconverter.in/index.php?q=Devanagari-to-IPA
+
+    val wordlistFrench = spark.read.option("encoding","utf8").option("header","true").csv("frenchwordlist - 50.csv").
+      filter(col("gender") =!= " None")
+
+    val wordlistHindi = spark.read.option("encoding", "utf8").option("header", "true").
+      csv("hindiwordlist - 50.csv").
+      filter(col("gender")=!=" None")
+
+    wordlistFrench.select("translation").show(50)
     spark.stop()
   }
 }
